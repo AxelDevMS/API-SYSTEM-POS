42dff4fc5f5fce2dd45db8b730d1cc2bf64373ed
AxelDevMS <asmtel2001@gmail.com>
2025-07-17 19:03:50 -0600

    creación proyecto

---
+++ src/test/java/com/asmdev/api/pos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asmdev.api.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
