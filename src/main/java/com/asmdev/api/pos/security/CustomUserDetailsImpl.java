a4f399668b382be8c04c4ed8e5c8b65eba138dd7
AxelDevMS <asmtel2001@gmail.com>
2025-09-02 17:55:06 -0600

    implementación clase userDetailServiceImpl

---
+++ src/main/java/com/asmdev/api/pos/security/CustomUserDetailsImpl.java
@@ -0,0 +1,71 @@
+package com.asmdev.api.pos.security;
+
+import com.asmdev.api.pos.persistence.entity.PermissionEntity;
+import com.asmdev.api.pos.persistence.entity.RoleEntity;
+import com.asmdev.api.pos.persistence.entity.UserEntity;
+import com.asmdev.api.pos.utils.status.UserStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CustomUserDetailsImpl implements UserDetails {
+
+    @Autowired
+    private UserEntity userEntity;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        //agregar roles
+        RoleEntity roleEntity = this.userEntity.getRole();
+        authorities.add(new SimpleGrantedAuthority(roleEntity.getName()));
+        for (PermissionEntity permission: roleEntity.getPermissions() ){
+            authorities.add(new SimpleGrantedAuthority(String.valueOf(permission.getName())));
+        }
+
+        return authorities;
+
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.userEntity.getStatus().equals(UserStatus.ACTIVE);
+    }
+
+    public UserEntity user(){
+        return userEntity;
+    }
+}
