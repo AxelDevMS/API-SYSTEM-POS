45d0d153550bcaf743c29db8e7b30409306c2b72
AxelDevMS <asmtel2001@gmail.com>
2025-09-02 17:57:07 -0600

    implementación clase userDetailServiceImpl

--- src/main/java/com/asmdev/api/pos/security/CustomUserDetails.java
+++ src/main/java/com/asmdev/api/pos/security/CustomUserDetails.java
@@ -11,10 +11,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-public class CustomUserDetailsImpl implements UserDetails {
+public class CustomUserDetails implements UserDetails {
 
     @Autowired
     private UserEntity userEntity;
